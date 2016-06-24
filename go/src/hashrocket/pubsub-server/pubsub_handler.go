b5beb6721c06dc1f6a5648f8475c4ebbb85c4ac3
Jack Christensen <dev+jackc@hashrocket.com>
2016-06-24 15:28:00 -0500

    Semi-working Go server

--- go/src/hashrocket/pubsub-server/pubsub_handler.go
+++ go/src/hashrocket/pubsub-server/pubsub_handler.go
@@ -40,14 +40,45 @@ func (h *pubsubHandler) Accept(ws *websocket.Conn) {
 
 		switch msg.Type {
 		case "subscribe":
-			log.Println("subscribe")
+			h.subscribe(ws, msg.Payload["channel"])
 		case "sendMessage":
-			log.Println("sendMessage")
+			h.sendMessage(ws, msg.Payload["channel"], msg.Payload["message"])
 		}
 	}
 }
 
 func (h *pubsubHandler) subscribe(ws *websocket.Conn, channel string) {
+	h.mutex.Lock()
+
+	if conns, ok := h.channelsConns[channel]; ok {
+		h.channelsConns[channel] = append(conns, ws)
+	} else {
+		h.channelsConns[channel] = []*websocket.Conn{ws}
+	}
+
+	if chans, ok := h.connsChannels[ws]; ok {
+		h.connsChannels[ws] = append(chans, channel)
+	} else {
+		h.connsChannels[ws] = []string{channel}
+	}
+
+	h.mutex.Unlock()
+}
+
+func (h *pubsubHandler) sendMessage(ws *websocket.Conn, channel string, message string) {
+	h.mutex.Lock()
+
+	if conns, ok := h.channelsConns[channel]; ok {
+		for _, c := range conns {
+			if c != ws {
+				if err := websocket.JSON.Send(c, &WsMsg{Type: "message", Payload: map[string]string{"channel": channel, "message": message}}); err != nil {
+					log.Println("websocket.JSON.Send err:", err)
+				}
+			}
+		}
+	}
+
+	h.mutex.Unlock()
 }
 
 func (h *pubsubHandler) cleanup(ws *websocket.Conn) {
