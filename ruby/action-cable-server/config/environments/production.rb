69c2a8dc9595c4b7200c3ce7ea816092bcade367
Jack Christensen <dev+jackc@hashrocket.com>
2016-08-11 17:03:22 -0500

    Allow all request origins

--- ruby/action-cable-server/config/environments/production.rb
+++ ruby/action-cable-server/config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ /http:\/\/*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
