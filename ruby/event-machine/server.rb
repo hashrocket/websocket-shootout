16b2c9c4a27cca1c71de93f67fc5551b8c1303aa
Brad Grzesiak <brad@bendyworks.com>
2016-09-02 13:37:25 -0500

    Add Ruby Event Machine
    It feels a bit unfair to compare framework-baked WebSocket solutions
    against language-level WebSocket libraries. For example, it might be
    worth writing a non-Phoenix version for Elixir as well?

---
+++ ruby/event-machine/server.rb
@@ -0,0 +1,23 @@
+require 'em-websocket'
+require 'json'
+
+EM.run {
+  @channel = EM::Channel.new
+
+  EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
+    ws.onopen {
+      sid = @channel.subscribe {|msg| ws.send msg }
+      @channel.push "#{sid} connected"
+    }
+
+    ws.onmessage { |msg|
+      puts msg.inspect
+      cmd, payload = JSON(msg).values_at('type', 'payload')
+      if cmd == 'echo'
+        ws.send payload.to_json
+      else
+        @channel.push payload.to_json
+      end
+    }
+  end
+}
