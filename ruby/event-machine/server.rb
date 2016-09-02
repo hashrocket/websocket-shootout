e84d3682a1c8f0dc44b4c9718ae9070813c37a09
Jack Christensen <dev+jackc@hashrocket.com>
2016-09-02 14:25:42 -0500

    Add CLI options to event machine

--- ruby/event-machine/server.rb
+++ ruby/event-machine/server.rb
@@ -1,10 +1,26 @@
 require 'em-websocket'
 require 'json'
+require 'optparse'
+
+address = "0.0.0.0"
+port = 8080
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: bundle exec server.rb [options]"
+
+  opts.on("-a", "--address", "Address") do |a|
+    address = a
+  end
+
+  opts.on("-p", "--port PORT", Integer, "Port") do |p|
+    port = Integer(p)
+  end
+end.parse!
 
 EM.run {
   @channel = EM::Channel.new
 
-  EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
+  EM::WebSocket.run(:host => address, :port => port) do |ws|
     ws.onopen {
       sid = @channel.subscribe {|msg| ws.send msg }
       @channel.push "#{sid} connected"
